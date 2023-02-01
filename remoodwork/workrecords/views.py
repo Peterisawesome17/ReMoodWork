ecbb7d16f04d8d540c8c741039735bcaaf5608cf
petero <peterisawesome17@gmail.com>
2023-01-31 16:22:38 -0800

    Created and rendered the main home page of remoodwork website used in workrecords and its templates folder

--- remoodwork/workrecords/views.py
+++ remoodwork/workrecords/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse #Used for testing http response to view web pages of remoodwork's workrecords
+from django.http import HttpResponse # Used for testing http response to view web pages of remoodwork's workrecords - Peter
+from datetime import datetime # Useful for implementing pulse survey page later in this project - Peter
 
 # Create your views here.
 # Used for creating a main website of remoodwork and work record logs of an employee- Peter
@@ -8,8 +9,18 @@ def home_view(request):
     '''A view of a home website page of remoodwork'''
     # More details will be added later on
     # Default return to check an http reponse of a web page
-    return HttpResponse('<h1>ReMoodWork Home</h1>')
+    return render(request=request, template_name='workrecords/home_page.html', context={})
 
 def pulse_survey_view(request):
     '''A view of a pulse survey page where employee creates their work/activity record logs'''
-    return HttpResponse('<h1>ReMoodWork Pulse Survey</h1>')
+    #Update 1: Will be used to print out today's date of a work/activity record logs of an employee later on
+    date_today = datetime.today()
+    # {{date_today}} in the templates (later on) for context - Peter
+    return HttpResponse('<head>'
+                        '<title>ReMoodWork</title>'
+                        '</head>'
+                        '<body>'
+                        '<h1>ReMoodWork Pulse Survey</h1>'
+                        f"<p>Today's date {date_today}</p>"
+                        '</body>'
+                        )
