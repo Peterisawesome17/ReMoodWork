0f14e9bbfba76edeb98ac0033c316d175582d608
petero <peterisawesome17@gmail.com>
2023-01-31 19:59:10 -0800

    Created a dummy data of record logs to loop each work data log of an employee for the main home page of remoodwork website

--- remoodwork/workrecords/views.py
+++ remoodwork/workrecords/views.py
@@ -5,11 +5,43 @@ from datetime import datetime # Useful for implementing pulse survey page later 
 # Create your views here.
 # Used for creating a main website of remoodwork and work record logs of an employee- Peter
 # Will also be used to create pulse surveys to employees - Peter
+
+# Dummy data for how it will look like on the home page of remoodwork site - Peter
+log_records = [
+    {
+        'title': 'Wrote five test cases to cover additional features of an app',
+        'posted_date': '07/20/2023',
+        'category_type': 'Work task',
+        'number_of_hours': '5',
+        'location': 'San Diego',
+        'emoji_face': '\N{tired face}'
+    },
+    {
+        'title': 'Riding a short walk',
+        'posted_date': '08/13/2023',
+        'category_type': 'Taking a break',
+        'number_of_hours': '1',
+        'location': 'San Diego',
+        'emoji_face': '\N{grinning face}'
+    },
+    {
+        'title': 'Taking a trip to Spain',
+        'posted_date': datetime.today(),
+        'category_type': 'Vacation Trip',
+        'number_of_hours': '64',
+        'location': 'San Diego',
+        'emoji_face': '\N{smiling face with sunglasses}'
+    }
+]
+
 def home_view(request):
     '''A view of a home website page of remoodwork'''
     # More details will be added later on
     # Default return to check an http reponse of a web page
-    return render(request=request, template_name='workrecords/home_page.html', context={})
+    context =  {
+        'log_records': log_records
+    }
+    return render(request=request, template_name='workrecords/home_page.html', context=context)
 
 def pulse_survey_view(request):
     '''A view of a pulse survey page where employee creates their work/activity record logs'''
