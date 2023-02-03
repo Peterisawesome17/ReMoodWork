a546bbf071ae8973beda13f7f98abbf9e9de597b
petero <peterisawesome17@gmail.com>
2023-02-03 13:02:42 -0800

    Updated registration url routes users of remoodwork website

--- remoodwork/users/urls.py
+++ remoodwork/users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views # views imported from Current directory of workrecords - Peter
 
 urlpatterns = [
-    path('register/', views.register_view, name='remoodwork-users-register'),
+    # path('register/', views.register_view, name='remoodwork-users-register'),
 ]
