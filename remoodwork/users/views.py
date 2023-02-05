ce41c835cfdede1bdb8caeba537b187255e59b1d
petero <peterisawesome17@gmail.com>
2023-02-04 22:14:35 -0800

    Modified register view for creating users in remoodwork

--- remoodwork/users/views.py
+++ remoodwork/users/views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 # from django.contrib.auth.forms import UserCreationForm
-from users.forms import UserRegisterationForm
+from users.forms import UserRegistrationForm
+from django.contrib import messages
 
 # Create your views here.
 def register_view(request):
-    form = UserRegisterationForm()
-    context = {
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request=request, message=f'Account created for {username}')
+            return redirect('remoodwork-home')
+        else:
+            context = {
+                'form': form,
+            }
+            # return HttpResponse('<h1>Welcome to the employees registration page!</h1>')
+            return render(request=request, template_name='users/register_page.html', context=context)
+    else:
+        form = UserRegistrationForm()
+        context = {
         'form' : form,
-    }
-    # return HttpResponse('<h1>Welcome to the employees registration page!</h1>')
-    return render(request=request, template_name='users/register_page.html', context=context)
+        }
+        # return HttpResponse('<h1>Welcome to the employees registration page!</h1>')
+        return render(request=request, template_name='users/register_page.html', context=context)
