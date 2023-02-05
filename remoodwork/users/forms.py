ba61f8359ebb8fb31a4dd50d67cf2e197886687a
petero <peterisawesome17@gmail.com>
2023-02-04 22:12:24 -0800

    Added password hidden field and rename user register for remoodwork

--- remoodwork/users/forms.py
+++ remoodwork/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from users.models import User
-class UserRegisterationForm(forms.ModelForm):
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
         fields = ['username', 'password', 'first_name', 'last_name', 'email', 'company_name', 'job_classification_choice']
