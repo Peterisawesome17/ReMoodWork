36d8a3bdb32808383a49452dfd1bd8d8d6c1793b
petero <peterisawesome17@gmail.com>
2023-02-07 15:30:07 -0800

    Some comments of a user register in user admin.py of remoodwork

--- remoodwork/users/admin.py
+++ remoodwork/users/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from users.models import User
 
 # Register your models here.
+# Must provide a User register for the admin page of remoodwork
 admin.site.register(User)
