cb379c6bd8d224fa8a85b7af28dd25c6ea95098b
petero <peterisawesome17@gmail.com>
2023-02-07 18:33:03 -0800

    Commented out user registration save method for remoodwork

--- remoodwork/users/forms.py
+++ remoodwork/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from users.models import User
+from users.models import User, Employee
 class UserRegistrationForm(forms.ModelForm):
     ''' Used for producing a registration form for the user
     presented in an html register template page '''
@@ -7,3 +7,11 @@ class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password', 'full_name', 'email', 'company_name', 'job_classification_choice']
+
+    # def save(self, commit=True):
+    #     user_instance = super().save(commit=False)
+    #     # Must provide a custom implementation logic of creating a user and an employee
+    #     # (More will be touched upon later in this project)
+    #     if commit:
+    #         user_instance.save()
+    #         # employee = Employee(user=)
