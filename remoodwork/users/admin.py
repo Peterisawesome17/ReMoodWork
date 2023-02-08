7c3d84b18a76f65657ae34ca673dbc84ad576913
petero <peterisawesome17@gmail.com>
2023-02-08 10:48:19 -0800

    Added few comments of an admin page of users/employees for remoodwork

--- remoodwork/users/admin.py
+++ remoodwork/users/admin.py
@@ -3,6 +3,8 @@ from users.models import User, Employee
 
 # Register your models here.
 # Must provide a User and Employee register for the admin page of remoodwork
+# A User Admin class will be added later in this project.
+
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ('user', )
 
