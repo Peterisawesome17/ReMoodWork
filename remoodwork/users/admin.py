bf8f31573c797dd09ea49cbd4ab8fd537112309a
petero <peterisawesome17@gmail.com>
2023-02-08 10:45:31 -0800

    Displayed a list of employees by their username from user model and registered in the admin page for remoodwork

--- remoodwork/users/admin.py
+++ remoodwork/users/admin.py
@@ -3,5 +3,8 @@ from users.models import User, Employee
 
 # Register your models here.
 # Must provide a User and Employee register for the admin page of remoodwork
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('user', )
+
 admin.site.register(User)
-admin.site.register(Employee)
+admin.site.register(Employee, EmployeeAdmin)
