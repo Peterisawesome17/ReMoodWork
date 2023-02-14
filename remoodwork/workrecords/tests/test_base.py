f555169dd9a2e5c8bf0cab71b7a68c998fcde4a5
petero <peterisawesome17@gmail.com>
2023-02-13 23:32:53 -0800

    Started creating test cases of pulse survey models in remoodwork.

---
+++ remoodwork/workrecords/tests/test_base.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.core.management import call_command
+
+
+class WorkRecordsTestCaseCounter(TestCase):
+    ''' A test case basis to count all the passed test cases used to
+    test workrecords app for remoodwork '''
+
+    @classmethod
+    def setUpClass(cls):
+        ''' Initializes a set-up to count a number of test cases passed'''
+        super(WorkRecordsTestCaseCounter, cls).setUpClass()
+        cls.count = 0
+
+    @classmethod
+    def countNum(cls):
+        ''' Helper and mutator method to keep counting the number of test cases that were passed
+        for each test suite script files made for the workrecords app of remoodwork '''
+        cls.count += 1
+
+    @classmethod
+    def getNumOfTestCases(cls):
+        ''' Helper and a getter method to count a total number of test cases that were passed for
+        each test suite script files made for the workrecords app of remoodwork '''
+        return cls.count
+
+    @classmethod
+    def tearDownClass(cls):
+        ''' Used to display the total number of test cases passed with a complete percentage
+        number for measuring each test suite of the workrecords app for remoodwork.
+        It also displays the amount of failed test cases presented in all test cases
+        covered for each test suite of the workrecords app for remoodwork. '''
+        call_command('flush', interactive=False)
+        test_methods = set(test_method for test_method in cls.__dict__ if 'test' in test_method)
+        tot_num_test_methods = len(test_methods)
+        print(f'Ran {tot_num_test_methods} number of test cases in '
+              f'{(cls.getNumOfTestCases() / tot_num_test_methods) * 100}% test suite of {cls.__name__}')
+        num_of_failed_test_cases = tot_num_test_methods - cls.getNumOfTestCases()
+        if num_of_failed_test_cases:
+            print(f'Number of failed test cases occuring {num_of_failed_test_cases}')
---
+++ remoodwork/workrecords/tests/test_workrecord_models.py
@@ -0,0 +1,43 @@
+from users.models import User, Employee
+from workrecords.models import PulseSurvey
+from workrecords.tests.test_base import WorkRecordsTestCaseCounter
+
+class WorkRecordsTestCases(WorkRecordsTestCaseCounter):
+    @classmethod
+    def setUpClass(cls):
+        ''' Sets up test cases for creating a pulse survey record of an employee model,
+        where an employee shall be allowed to create one or more pulse survey records
+        on remoodwork website. '''
+        super(WorkRecordsTestCases, cls).setUpClass()
+        cls.employee = cls._create_an_employee()
+        cls.employee.save()
+
+    @classmethod
+    def _create_an_employee(cls):
+        username = 'mikerandy'
+        password = 'anothertest555!'
+        full_name = 'Mike Randy'
+        email = 'mikerandy@test.com'
+        company_name = 'Amazon'
+        job_classification_choice = 'EMPLOYEE'
+        user = User(
+            username=username,
+            password=password,
+            full_name=full_name,
+            email=email,
+            company_name=company_name,
+            job_classification_choice=job_classification_choice
+        )
+        user.save()
+        employee = Employee(user=user)
+        return employee
+
+    def test_one_employee(cls):
+        cls.assertEqual(1, len(Employee.objects.all()))
+        cls.countNum()
+
+    @classmethod
+    def tearDownClass(cls):
+        ''' Invokes teardownClass method from test_base.py file of all
+        test suites of the workrecords app for remoodwork. '''
+        super().tearDownClass()
