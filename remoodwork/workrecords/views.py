5b545eaff18e1cd28a57b62bf61145830d741db6
petero <peterisawesome17@gmail.com>
2023-02-16 21:14:20 -0800

    Modified home view and web page template for remoodwork.

--- remoodwork/workrecords/views.py
+++ remoodwork/workrecords/views.py
@@ -4,45 +4,24 @@ from datetime import datetime # Useful for implementing pulse survey page later 
 from workrecords.forms import PulseSurveyCreationForm
 from users.models import Employee, User
 from django.contrib import messages
+from workrecords.models import PulseSurvey
 
 # Create your views here.
 # Used for creating a main website of remoodwork and work record logs of an employee- Peter
 # Will also be used to create pulse surveys to employees - Peter
 
-# Dummy data for how it will look like on the home page of remoodwork site - Peter
-log_records = [
-    {
-        'title': 'Wrote five test cases to cover additional features of an app',
-        'posted_date': '07/20/2023',
-        'category_type': 'Work task',
-        'number_of_hours': '5',
-        'location': 'San Diego',
-        'emoji_face': '\N{tired face}'
-    },
-    {
-        'title': 'Riding a short walk',
-        'posted_date': '08/13/2023',
-        'category_type': 'Taking a break',
-        'number_of_hours': '1',
-        'location': 'San Diego',
-        'emoji_face': '\N{grinning face}'
-    },
-    {
-        'title': 'Taking a trip to Spain',
-        'posted_date': datetime.today(),
-        'category_type': 'Vacation Trip',
-        'number_of_hours': '64',
-        'location': 'San Diego',
-        'emoji_face': '\N{smiling face with sunglasses}'
-    }
-]
-
 def home_view(request):
     '''A view of a home website page of remoodwork'''
     # More details will be added later on
     # Default return to check an http reponse of a web page
+    pulse_survey_records = None
+    if request.user.id:
+        user = User.objects.get(pk=request.user.id)
+        employee = Employee.objects.get(user=user)
+        pulse_survey_records = PulseSurvey.objects.filter(employee=employee)
     context =  {
-        'log_records': log_records
+        'user_id': request.user.id,
+        'pulse_survey_records': pulse_survey_records
     }
     return render(request=request, template_name='workrecords/home_page.html', context=context)
 
