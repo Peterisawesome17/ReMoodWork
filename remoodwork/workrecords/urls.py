4b854a5ca0019a6f6c8a4c86c8fa6303aaa47fcd
petero <peterisawesome17@gmail.com>
2023-02-26 21:54:01 -0800

    Added new users url made specifically for an employer for remoodwork.

--- remoodwork/workrecords/urls.py
+++ remoodwork/workrecords/urls.py
@@ -4,5 +4,6 @@ from . import views # views imported from Current directory of workrecords - Pet
 urlpatterns = [
     path('', views.home_view, name='remoodwork-home'),
     path('employee/<int:pk>/pulsesurvey/', views.pulse_survey_view, name='remoodwork-pulse-survey'),
+    path('employee/<int:pk>/pulsesurvey/employer/<int:emp_pk>/', views.pulse_survey_view, name='remoodwork-pulse-survey'),
     path('employee/<int:pk>/createpulsesurvey/', views.create_pulse_survey_view, name='remoodwork-create-pulse-survey')
 ]
