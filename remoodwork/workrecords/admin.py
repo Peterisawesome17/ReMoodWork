4c6a1c5144f52d0f2ea01cad107de4553d2c2e3c
petero <peterisawesome17@gmail.com>
2023-04-14 21:20:07 -0700

    Added meal plan register in admin remoodwork

--- remoodwork/workrecords/admin.py
+++ remoodwork/workrecords/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from workrecords.models import PulseSurvey
+from workrecords.models import PulseSurvey, MealPlan
 
 # Register your models here.
 # Must provide a PulseSurvey register for the admin page of remoodwork
 # More models will be registered later
 admin.site.register(PulseSurvey)
+admin.site.register(MealPlan)
