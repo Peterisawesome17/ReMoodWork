c590f9e4a3b684ea64325471a877759e566c7fb6
petero <peterisawesome17@gmail.com>
2023-04-27 00:35:40 -0700

    Added order food meal item url for remoodwork.

--- remoodwork/workrecords/urls.py
+++ remoodwork/workrecords/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('employee/<int:pk>/createpulsesurvey/', views.create_pulse_survey_view, name='remoodwork-create-pulse-survey'),
     path('employee/<int:pk>/mealplan/', views.meal_plan_view, name='remoodwork-meal-plan'),
     path('employee/<int:pk>/createmealplan/', views.create_meal_plan_view, name='remoodwork-create-meal-plan'),
-    path('employer/<int:emp_pk>/createfooditem/', views.create_food_item_view, name='remoodwork-create-food-item')
+    path('employer/<int:emp_pk>/createfooditem/', views.create_food_item_view, name='remoodwork-create-food-item'),
+    path('employee/<int:pk>/ordermeal/<int:food_pk>', views.order_food_meal_item, name='remoodwork-order-meal')
 ]
