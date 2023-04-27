4863bbc67124838c3a7b58d10aa53a588781bfab
petero <peterisawesome17@gmail.com>
2023-04-27 00:34:53 -0700

    Registered FoodItem and Order for admin remoodwork.

--- remoodwork/workrecords/admin.py
+++ remoodwork/workrecords/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from workrecords.models import PulseSurvey, MealPlan
+from workrecords.models import PulseSurvey, MealPlan, FoodItem, Order
 
 # Register your models here.
 # Must provide a PulseSurvey register for the admin page of remoodwork
 # More models will be registered later
 admin.site.register(PulseSurvey)
 admin.site.register(MealPlan)
+admin.site.register(FoodItem)
+admin.site.register(Order)
