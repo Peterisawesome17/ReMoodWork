f37dd70a948bf57fd7198c42fb55cfba53cd968b
petero <peterisawesome17@gmail.com>
2023-05-01 15:15:18 -0700

    Rechanged and modified food item view contents in remoodwork.

--- remoodwork/workrecords/tests/test_food_item_creation_view.py
+++ remoodwork/workrecords/tests/test_food_item_creation_view.py
@@ -4,13 +4,12 @@ from django.test import Client
 from django.urls import reverse
 from workrecords.models import FoodItem
 
-class FoodItemandOrderCreationViewTestCase(WorkRecordsTestCaseCounter):
+class FoodItemCreationViewTestCase(WorkRecordsTestCaseCounter):
     @classmethod
     def setUpClass(cls):
         ''' Sets up test cases on testing view responses of a food item creation
         produced from an employer '''
-        cls.user = cls._create_an_user()
-        cls.employer = Employer(user=cls.user)
+        cls.employer = cls._create_an_employer()
         cls.employer.save()
         cls.client = Client()
 
@@ -18,8 +17,8 @@ class FoodItemandOrderCreationViewTestCase(WorkRecordsTestCaseCounter):
         ''' Test 1: Valid test case for users(employers) responding
         to create food item (Must be in create_food_item_page.html) '''
         login_response = cls.client.post(reverse('remoodwork-login-user'),
-                                         {'username': cls.user.username,
-                                          'password': 'anothertest666!'})
+                                         {'username': cls.employer.user.username,
+                                          'password': 'testingemployer123!'})
         cls.assertEqual(302, login_response.status_code)
         cls.assertRedirects(login_response, reverse('remoodwork-home'))
         create_food_item_response = cls.client.get(reverse('remoodwork-create-food-item',
@@ -30,7 +29,7 @@ class FoodItemandOrderCreationViewTestCase(WorkRecordsTestCaseCounter):
         cls.assertTemplateUsed(response=create_food_item_response,
                                template_name='workrecords/create_food_item_page.html')
         create_food_item_restaurant_data_form = {
-            'food_name': 'Smoked salmon',
+            'food_name': 'Smoked Salmon',
             'description': 'Cooked and marinated with lemon juice',
             'price': 20.00,
             'cuisine_type': 'american',
@@ -51,5 +50,16 @@ class FoodItemandOrderCreationViewTestCase(WorkRecordsTestCaseCounter):
         cls.assertEqual(1, len(FoodItem.objects.all()))
         cls.assertRedirects(response=food_item_restaurant_created_response,
                             expected_url=reverse('remoodwork-home'))
-        food_item_in_home_page_response = cls.client.get(reverse('remoodwork-home'))
+        food_item_in_home_page_response = cls.client.get(reverse('remoodwork-home',
+                                                                 kwargs={}))
         cls.assertEqual(200, food_item_in_home_page_response.status_code)
+        cls.assertContains(food_item_in_home_page_response, "Food name: Smoked Salmon")
+        cls.assertContains(food_item_in_home_page_response,
+                           "Description: Cooked and marinated with lemon juice")
+        cls.assertContains(food_item_in_home_page_response, "Price: 20.0")
+        cls.assertContains(food_item_in_home_page_response, "Cuisine: American")
+        cls.assertContains(food_item_in_home_page_response, "Type: Restaurant")
+        cls.assertContains(food_item_in_home_page_response, "Restaurant name: Dr.Dock")
+        cls.assertContains(food_item_in_home_page_response, "Calories: 200")
+        cls.assertContains(food_item_in_home_page_response, "Dietary Restrictions: Gluten-free")
+        cls.assertContains(food_item_in_home_page_response, "Allergy: Shellfish")
