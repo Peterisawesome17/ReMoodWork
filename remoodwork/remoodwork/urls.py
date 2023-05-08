d060be475842f7a1801a86ad64c5a1b0ad6a0365
petero <peterisawesome17@gmail.com>
2023-05-07 19:07:30 -0700

    Added media url and root for uploading food item images on remoodwork.

--- remoodwork/remoodwork/urls.py
+++ remoodwork/remoodwork/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views, views as auth_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,5 @@ urlpatterns = [
     path('logout/', auth_view.logout_view, name='remoodwork-logout-user'),
     path('', include('users.urls'))
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
